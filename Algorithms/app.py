58a249a5816a7b8798bd57016fe5b96c68763a7f
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2025-01-03 19:47:40 +0530

    Add files via upload

---
+++ Algorithms/app.py
@@ -0,0 +1,22 @@
+def bubble_sort(arr):
+    size = len(arr)
+    for i in range(size - 1):
+        swapped = False
+        for j in range(size - 1 - i):  
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]  
+                swapped = True
+        if not swapped: 
+            break
+    return arr 
+    
+    return arr          
+             
+
+if __name__ == '__main__':
+    arr = [99, 0, 5, 20, 123, 0, -1, 72, 21, 22, 13, 8, 7, 67, 29, 1, 2, 4, -20]
+    arr1 = ["mona", "dhaval", "aamir", "tina", "chang"]
+
+    print(bubble_sort(arr))
+    print(bubble_sort(arr1))
+    
