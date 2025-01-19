4e26a186d76b8099c751678af5fa4ffaa48ce130
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2025-01-19 22:53:51 +0530

    Binary Search.py

--- Binary Search/Binary Search.py
+++ Binary Search/Binary Search.py
@@ -1,1 +1,22 @@
 
+def binary_search(a, target):
+    n = len(a)
+    low = 0
+    high = n-1
+
+    while low <= high:
+        mid = (low+high)//2
+        if a[mid] == target:
+            return mid
+        elif target>a[mid]:
+            low = mid + 1
+        else:
+            high = mid - 1
+             
+    return -1
+
+if __name__ == "__main__":
+    a = [3, 4, 6, 7, 9, 12, 16, 17]
+    target = 6
+    print(binary_search(a, target))
+    
