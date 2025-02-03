3a64aab76878f0d3d64d9292328be2079cbc0128
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2025-02-03 19:32:11 +0530

    array.py

--- Arrays/Easy/Find missing number in an array.py
+++ Arrays/Easy/Find missing number in an array.py
@@ -9,3 +9,11 @@ def find_missing_number(arr,n):
 arr = [1, 2, 4, 6, 7, 9]  
 n = 9
 print("Missing number:", find_missing_number(arr,n))
+
+
+
+arr = [14, 15, 20, 30, 31, 45]
+print(sorted(arr))
+a = set(range(0, max(arr)))
+missing = a - set(arr)
+print(missing)
