cc0c47fdc5f86561cc3900df3a1616c807aa2de9
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2025-02-05 11:02:02 +0530

    Create basic.py

---
+++ OOPS/basic.py
@@ -0,0 +1,24 @@
+class Dog:
+    # Class attribute (shared by all instances)
+    species = "Canis familiaris"
+
+    # Constructor method (initializes instance attributes)
+    def __init__(self, name, age):
+        self.name = name  # Instance attribute
+        self.age = age    # Instance attribute
+
+    # Instance method
+    def bark(self):
+        return f"{self.age} says woof!"
+
+# Create two objects (instances) of the Dog class
+dog1 = Dog("Buddy", 3)
+dog2 = Dog("Max", 5)
+
+# Access attributes
+print(dog1.name)  # Output: Buddy
+print(dog2.age)   # Output: 5
+
+# Call methods
+print(dog1.bark())  # Output: Buddy says woof!
+print(dog2.bark())  # Output: Max says woof!
