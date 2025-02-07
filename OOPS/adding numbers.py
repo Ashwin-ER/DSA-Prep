9a2432f84cab1cc931331c7d1d47b4b867dd4753
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2025-02-07 16:37:27 +0530

    Update adding numbers.py

--- OOPS/adding numbers.py
+++ OOPS/adding numbers.py
@@ -12,3 +12,20 @@ marks.tam=50
 marks.eng=70
 marks.mat=100
 print(marks.total())
+
+
+###22222
+class node:
+    name=None
+    age=None
+    def sentence(self):
+        print(self.name+" is " + self.age +" years old")
+
+node1 = node()
+node1.name="ashwin"
+node1.age='20'
+node2=node()
+node2.name="ash"
+node2.age='15'
+node1.sentence()
+node2.sentence()
