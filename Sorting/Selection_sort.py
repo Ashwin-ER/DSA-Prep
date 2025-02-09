956eedba60110e8a66ea28d74e3935b279645dc5
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2025-02-09 19:55:15 +0530

    Update Selection_sort.py

--- Sorting/Selection_sort.py
+++ Sorting/Selection_sort.py
@@ -1,16 +1,11 @@
 def selection_sort(arr):
-    #min = i
-    size = len(elements)
-    for i in range(size):
-        min = i
-        for j in range(i+1, size):
-            if arr[min] > arr[j]:
-                min = j
-        if i != min: 
-            arr[min] , arr[i] = arr[i] , arr[min]
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+                arr[i], arr[min_index] = arr[min_index], arr[i]
     return arr
-
-
-if __name__ == '__main__':
-    elements = [89, 78, 61, 53, 23, 21, 17, 12, 9, 7, 6, 78,7 ,2, 2, 1]
-    print(selection_sort(elements))
+arr = [89, 78, 61, 53, 23, 21, 17, 12, 9, 7, 6, 78,7 ,2, 2, 1]
+print(selection_sort(arr))
