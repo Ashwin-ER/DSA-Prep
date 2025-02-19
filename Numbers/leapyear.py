124c766af4c2df608c185db92c44361214b144a7
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2025-02-20 01:32:53 +0530

    Update leapyear.py

--- Numbers/leapyear.py
+++ Numbers/leapyear.py
@@ -1,1 +1,11 @@
 
+def leapyear(n):
+    if n%4==0 and n%100!=0:
+        print("leap")
+    else:
+        print('not a leap')
+
+
+if __name__ == "__main__":
+    n = 12315
+    print(leapyear(n))
