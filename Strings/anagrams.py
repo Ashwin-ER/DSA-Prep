eeb3aeb0f93bdaca7803ae699717127222094f4a
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2025-03-01 23:53:21 +0530

    Update anagrams.py

--- Strings/anagrams.py
+++ Strings/anagrams.py
@@ -11,3 +11,24 @@ if __name__ == "__main__":
     s1 = 'geeks'  
     s2 = 'kseeg'
     anagrams(s1,s2)
+
+
+#leetcode 
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if sorted(s) == sorted(t):
+            return True
+        else:
+            return False
+
+
+solution = Solution()
+s = "anagram"
+t = "nagaram"
+result = solution.isAnagram(s, t)
+print(result) 
